a03449f3d99681b7f5f2987ca33739005f216ac3
matthewdias <matthewdias@me.com>
2018-03-22 20:31:54 -0500

    Add viewpager

--- app/src/main/java/me/matthewdias/smartmirrorsettings/MainActivity.java
+++ app/src/main/java/me/matthewdias/smartmirrorsettings/MainActivity.java
@@ -3,33 +3,55 @@ package me.matthewdias.smartmirrorsettings;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    BottomNavigationView bottomNavigationView;
+    private ViewPager viewPager;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    LocationFragment locationFragment;
+    CalendarFragment calendarFragment;
+    MenuItem prevMenuItem;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_location:
-                    mTextMessage.setText(R.string.title_location);
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_calendar:
-                    mTextMessage.setText(R.string.title_calendar);
+                    viewPager.setCurrentItem(1);
                     return true;
 //                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
+//                    viewPager.setCurrentItem(2);
 //                    return true;
             }
             return false;
         }
+    };
 
+    private ViewPager.OnPageChangeListener pageListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+        @Override
+        public void onPageScrollStateChanged(int state) {}
+
+        @Override
+        public void onPageSelected(int position) {
+            if (prevMenuItem != null) {
+                prevMenuItem.setChecked(false);
+            } else {
+                bottomNavigationView.getMenu().getItem(0).setChecked(false);
+            }
+            bottomNavigationView.getMenu().getItem(position).setChecked(true);
+            prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+        }
     };
 
     @Override
@@ -37,9 +59,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.addOnPageChangeListener(pageListener);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        locationFragment = new LocationFragment();
+        adapter.addFragment(locationFragment);
+        calendarFragment = new CalendarFragment();
+        adapter.addFragment(calendarFragment);
+        viewPager.setAdapter(adapter);
     }
 
 }
